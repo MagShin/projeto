f163d51534cfe352cd3412d11f5b021a0ed89f5b
MagShin <cm417710@outlook.com>
2024-10-17 14:47:57 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col">
+        <p>© 2022. Todos os direitos reservados</p>
+        <p>Desenvolvido por: .... </p>
+        <p>endereço: Rua ....</p>
+    </div>
+</div>
+
+</div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col">
+        <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="register.php">Cadastro ADM</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="register_user.php">Cadastro usuario</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+        </ul>
+    </div>
+</div>
